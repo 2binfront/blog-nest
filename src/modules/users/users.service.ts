122352d1898199c5fdef9aa0b40e1c4f1cf515a4
胡含 <2binfront@gmail.com>
2024-12-20 18:02:34 +0800

    ✨ Feat: 新增密码环境变量配置

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 // 这应该是一个真正的类/接口，代表一个用户实体
 export type User = any;
 
 @Injectable()
 export class UsersService {
+  constructor(private config: ConfigService) {
+    if (this.config.get('PASSWORD')) this.users[0].password = this.config.get('PASSWORD');
+  }
   private readonly users = [
     {
       userId: 1,
